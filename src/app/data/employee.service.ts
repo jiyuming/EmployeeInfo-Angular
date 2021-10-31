c9a0dfa19b48f01067e852154ea2b826f5907446
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-10-30 23:49:27 -0400

    updated

--- src/app/data/employee.service.ts
+++ src/app/data/employee.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
-import { Observable } from 'rxjs/Observable';
-import { Employee} from './employee';
-import { EmployeeRaw} from './employee-raw';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { EmployeeRaw } from './employeeRaw';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class EmployeeService {
 
-  private url = "https://rocky-retreat-80791.herokuapp.com";
+  private url = 'https://teams-api-21679.herokuapp.com';
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
-  getEmployees(): Observable<Employee[]>{
+  getEmployees() {
     return this.http.get<Employee[]>(`${this.url}/employees`);
   }
 
-  saveEmployee(employee: EmployeeRaw): Observable<any>{
-    return this.http.put<any>(this.url + "/employee/" + employee._id, employee);
+  // add new EmployeeRaw record
+  saveEmployee(employee: EmployeeRaw): Observable<any> {
+    return this.http.put<any>(`${this.url}/employee/${employee._id}`, employee);
   }
 
-  getEmployee(id): Observable<EmployeeRaw[]>{
-    return this.http.get<EmployeeRaw[]>(this.url + "/employee-raw/" + id);
+  // get a EmployeeRaw[]
+  getEmployee(id): Observable<EmployeeRaw[]> {
+    return this.http.get<EmployeeRaw[]>(`${this.url}/employee-raw/${id}`)
   }
 }
