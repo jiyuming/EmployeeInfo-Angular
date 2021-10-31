1df4c87a279c0b3eb085ecf8b30c4f5c52de0695
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-10-30 23:53:34 -0400

    cleanup and add comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PositionsComponent } from './positions/positions.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PositionComponent } from './position/position.component';
 
-//import all created components
-import { HomeComponent} from "./home/home.component";
-import { EmployeesComponent} from "./employees/employees.component";
-import { PositionsComponent} from "./positions/positions.component";
-import { PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-
-import {EmployeeComponent} from './employee/employee.component';
-import {PositionComponent} from './position/position.component';
-
-const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'employees', component: EmployeesComponent},
-  {path: 'positions', component: PositionsComponent},
-  {path: 'employee/:_id', component: EmployeeComponent},
-  {path: 'position/:_id', component: PositionComponent},
-  {path: '', redirectTo:'/home', pathMatch:"full"},
-  {path: '**', component: PageNotFoundComponent}
+const routes: Routes = [{ path: 'home', component: HomeComponent },
+{ path: 'employees', component: EmployeesComponent },
+{ path: 'positions', component: PositionsComponent },
+{ path: 'employee/:_id', component: EmployeeComponent },
+{ path: 'position/:_id', component: PositionComponent },
+{ path: '', redirectTo: '/home', pathMatch: "full" },
+{ path: '**', component: PagenotfoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { PositionsComponent } from './positions/positions.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { EmployeeService } from './data/employee.service';
 import { PositionService } from './data/position.service';
-import { PositionComponent } from './position/position.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { PositionComponent } from './position/position.component';
 import { FormsModule} from '@angular/forms';
 
 @NgModule({
@@ -22,12 +22,12 @@ import { FormsModule} from '@angular/forms';
     NavComponent,
     ContentComponent,
     FooterComponent,
-    PageNotFoundComponent,
     HomeComponent,
     EmployeesComponent,
     PositionsComponent,
-    PositionComponent,
+    PagenotfoundComponent,
     EmployeeComponent,
+    PositionComponent,
   ],
   imports: [
     BrowserModule,
